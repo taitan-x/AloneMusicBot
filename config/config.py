ee19e90bc07c21b18119e7826936c3a3192d97f4
taitan-x <138150134+taitan-x@users.noreply.github.com>
2023-07-16 19:06:13 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -40,7 +40,7 @@ SONG_DOWNLOAD_DURATION = int(
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID", ""))
 
 # A name for your Music bot.
-MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "𝐑αηι ✘ 𝐌υsι¢")
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Rani")
 
 # Your User ID.
 OWNER_ID = list(
